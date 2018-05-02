b0e6343a404aed4765ab8b798f0bef218ed5f960
mhkkr <mokuhen@gmail.com>
2018-05-02 16:37:01 +0900

    iframe に対応しました！
    https://github.com/mhkkr/Google-Analytics-Organic-Search/pull/1
    ご指摘をいただき対応させていただきました。ありがとうございます！

--- add.js
+++ add.js
@@ -4,128 +4,155 @@
 
 javascript:
 
-(function(d){function l(){(function($){
+(function (d){function l(){(function ($){
+  'use strict';
 
-if ( $('#ID-m-content-content > h2').text() === 'オーガニック検索ソース' ) {
+  const $body = $('body');
 
-  if ( ! $('body').hasClass('ga_search_data_current') ) { // 連動[1] - 二度押し防止
+  // アカウントによっては iframe ではないかもしれない...ので念のため。
+  let $target = $body;
+  if ($('#galaxyIframe').contents().length) {
+    $target = $('#galaxyIframe').contents();
+  }
 
-    $('body')
-      .addClass('ga_search_data_current') // 連動[1] - 二度押し防止
-      .css({'position': 'relative'})
-      .append('<div style="background-color: rgba(0, 0, 0, .5); height: 100%; position: fixed; left: 0; top: 0; width: 99%; z-index: 999999999;"><div style="background-color: #fff; border-radius: 9px; border: 3px solid #333; padding: 20px; position: absolute; left: 50%; top: 50%; transform: translate(-50%, -50%);"><h1 style="margin: 0; text-align: center">オーガニック検索ソース[登録]</h1><div id="ga_search_data_state" style="margin-top: 1em; text-align: center;">このまましばらくお待ちください。</div><div style="background-color: #fff; border: 1px solid #ccc; margin: 1em auto 0; overflow: hidden; position: relative; width: 300px;"><div id="ga_search_data_bar" style="background-color: #ddd; display: inline-block; height: 100%; position: absolute; left: 0; top: 0; transition: width .4s;"></div><div style="padding: .25em; position: relative; text-align: center; z-index: 9;"><span id="ga_search_data_now">1</span>/<span id="ga_search_data_max">35</span></div></div><div style="margin-top: 1em; text-align: center;"><a href="#" id="ga_search_data_cancel">キャンセル</a></div></div></div>')
-    ;
+  if (location.href.indexOf('/trackingorgainc-search-sources/') != -1 && $target.find('#ID-m-content-content > h2').text() === 'オーガニック検索ソース') {
 
-    var s = 1000, // 処理待機時間
-        mv = 37,  // 登録最大個数
-        $e = $('#ga_search_data_state'),
-        $r = $('#ga_search_data_bar'),
-        $n = $('#ga_search_data_now'),
-        $m = $('#ga_search_data_max'),
-        $c = $('#ga_search_data_cancel')
-    ;
+    if (!$body.hasClass('ga_search_data_current')) { // 連動[1] - 二度押し防止
 
-    var roop;
-    function roopDo() {
-      roop = setInterval(function() { action(); }, s);
-    }
+      $body
+        .addClass('ga_search_data_current') // 連動[1] - 二度押し防止
+        .css({'position': 'relative'})
+        .append('<div style="background-color: rgba(0, 0, 0, .5); height: 100%; position: fixed; left: 0; top: 0; width: 99%; z-index: 999999999;"><div style="background-color: #fff; border-radius: 9px; border: 3px solid #333; padding: 20px; position: absolute; left: 50%; top: 50%; transform: translate(-50%, -50%);"><h1 style="margin: 0; text-align: center">オーガニック検索ソース[登録]</h1><div id="ga_search_data_state" style="margin-top: 1em; text-align: center;">このまましばらくお待ちください。</div><div style="background-color: #fff; border: 1px solid #ccc; margin: 1em auto 0; overflow: hidden; position: relative; width: 300px;"><div id="ga_search_data_bar" style="background-color: #ddd; display: inline-block; height: 100%; position: absolute; left: 0; top: 0; transition: width .4s;"></div><div style="padding: .25em; position: relative; text-align: center; z-index: 9;"><span id="ga_search_data_now">1</span>/<span id="ga_search_data_max">35</span></div></div><div style="margin-top: 1em; text-align: center;"><a href="#" id="ga_search_data_cancel">キャンセル</a></div></div></div>')
+      ;
 
-    action();
-    function action() {
-      clearInterval(roop);
+      // 設定
+      const time = 1000, // 処理待機時間
+            max = 37 // 登録最大個数
+      ;
 
-      if ( document.querySelector('input[value="+ 検索エンジンを追加"]') ) {
-        var $b = $('input[value="+ 検索エンジンを追加"]'),
-            $a = $('tbody.ID-adminTableBody'),
-            $t = $a.children('tr'),
-            l = $t.filter('[class*="ID-row-"]').length,
-            np = Math.floor((l / mv) * 100)
-        ;
-        $m.text(mv);
-        $r.width(np + '%');
-        $n.text(l);
-        $c.click(function(e) {
-          location.reload();
-          e.preventDefault();
-        });
-        function d(d1, d2, d3) {
-          $b.click();
-          setTimeout(function() {
-            poporon();
-            function poporon() {
-              if ( $('#ID-m-content-content input[type="text"]').length === 0 ) {
-                // 実行が早すぎた時の処理
-                setTimeout(function() {
-                  poporon();
-                }, s);
-              } else
-              {
-                $d = document.forms[0].elements;
-                $d[0].value = d1; $d[1].value = d2; $d[2].value = d3; $d[4].click();
-                roopDo();
-              }
+      // 差し込みHTMLを取得
+      const $ga_state  = $('#ga_search_data_state'),
+            $ga_bar    = $('#ga_search_data_bar'),
+            $ga_now    = $('#ga_search_data_now'),
+            $ga_max    = $('#ga_search_data_max'),
+            $ga_cancel = $('#ga_search_data_cancel')
+      ;
+
+      // ループ設定
+      let roop;
+      function roopContainer () {
+        roop = setInterval(function () { action(); }, time);
+      }
+
+      // 登録処理
+      function register (name, domain, query) {
+        setTimeout(function () {
+          insert(name, domain, query);
+        }, time);
+      }
+
+      // 挿入
+      function insert (name, domain, query) {
+        if ($target.find('#ID-m-content-content input[type="text"]').length === 0) {
+          // 実行が早すぎた時の処理
+          setTimeout(function () {
+            insert(name, domain, query);
+          }, time);
+        } else {
+          $target.find('#ID-m-content-content input[data-name="displayName"]').val(name);
+          $target.find('#ID-m-content-content input[data-name="domainName"]').val(domain);
+          $target.find('#ID-m-content-content input[data-name="queryParam"]').val(query);
+          $target.find('#ID-m-content-content button[data-name="actionFormButton"]').trigger('click');
+          roopContainer();
+        }
+      }
+
+      // 動作処理
+      function action () {
+        clearInterval(roop);
+
+        if ($target.find('#ID-m-content-content input[value="+ 検索エンジンを追加"]') && $target.find('#ID-m-content-content input[value="+ 検索エンジンを追加"]').is(':visible')) {
+          const $btn   = $target.find('#ID-m-content-content input[value="+ 検索エンジンを追加"]'),
+                $tbody = $target.find('#ID-m-content-content tbody.ID-adminTableBody'),
+                $tr    = $tbody.children('tr'),
+                length = $tr.filter('[class*="ID-row-"]').length,
+                rate   = Math.floor((length / max) * 100)
+          ;
+
+          $ga_max.text(max);
+          $ga_bar.width(rate + '%');
+          $ga_now.text(length);
+
+          if (length > max) {
+            $ga_state.css({'color': '#23945C', 'font-weight': 'bold'}).text('登録が完了しました！');
+            $ga_now.text(max);
+            $ga_cancel.text('閉じる');
+          } else {
+            $btn.trigger('click');
+            switch (length) {
+              case 0  : register("so-net", "so-net.ne.jp", "query"); break;
+              case 1  : register("nifty", "nifty.com", "Text"); break;
+              case 2  : register("nifty", "nifty.com", "q"); break;
+              case 3  : register("hi-ho", "hi-ho.excite.co.jp", "search"); break;
+              case 4  : register("odn", "odn.excite.co.jp", "search"); break;
+              case 5  : register("eonet", "eonet.excite.co.jp", "search"); break;
+              case 6  : register("excite", "excite.co.jp", "q"); break;
+              case 7  : register("livedoor", "livedoor.com", "q"); break;
+              case 8  : register("ocn", "ocn.ne.jp", "MT"); break;
+              case 9  : register("j:com", "search.myjcom.jp", "q"); break;
+              case 10 : register("sleipnir", "fenrir-inc.com", "q"); break;
+              case 11 : register("lunascape", "luna.tv", "keyword"); break;
+              case 12 : register("cybozu", "cybozu.net", "Keywords"); break;
+              case 13 : register("sony", "sony.jp", "MT"); break;
+              case 14 : register("fujitsu", "fmworld.net", "q"); break;
+              case 15 : register("fujitsu", "fmworld.net", "Text"); break;
+              case 16 : register("ecnavi", "ecnavi.jp", "Keywords"); break;
+              case 17 : register("pex", "pex.jp", "Keywords"); break;
+              case 18 : register("hao123", "hao123.com", "query"); break;
+              case 19 : register("ceron", "ceron.jp", "q"); break;
+              case 20 : register("お財布.com", "search.osaifu.com", "w"); break;
+              case 21 : register("warau.jp", "warau.jp", "search_word"); break;
+              case 22 : register("net-lavi", "netlavis.azione.jp", "q"); break;
+              case 23 : register("genieo", "search.genieo.com", "q"); break;
+              case 24 : register("funmoods search", "searchfunmoods.com", "q"); break;
+              case 25 : register("fooooo", "fooooo.com", "q"); break;
+              case 26 : register("便利コム", "benri.com", "Keywords"); break;
+              case 27 : register("スマート検索", "smart-search.jp", "Keywords"); break;
+              case 28 : register("jword", "jword.jp", "name"); break;
+              case 29 : register("jword", "jword.jp", "q"); break;
+              case 30 : register("aswidget", "aswidget.com", "Keywords"); break;
+              case 31 : register("wow", "jp.wow.com", "q"); break;
+              case 32 : register("adingo.jp", "adingo.jp", "Keywords"); break;
+              case 33 : register("tnc", "search.tnc.ne.jp", "q"); break;
+              case 34 : register("t-com", "search.t-com.ne.jp", "q"); break;
+              case 35 : register("jiqoo", "search.jiqoo.jp", "q"); break;
+              case 36 : register("auone", "sp-web.search.auone.jp", "q"); break;
+              case 37 : register("green goo", "green.search.goo.ne.jp", "MT"); break;
             }
-          }, s);
+          }
+        } else {
+          // 実行が早すぎた時の処理
+          setTimeout(function () {
+            action();
+          }, time);
         }
+      }
 
-        if ( l  === 0  ) { d("so-net", "so-net.ne.jp", "query"); } else
-        if ( l  === 1  ) { d("nifty", "nifty.com", "Text"); } else
-        if ( l  === 2  ) { d("nifty", "nifty.com", "q"); } else
-        if ( l  === 3  ) { d("hi-ho", "hi-ho.excite.co.jp", "search"); } else
-        if ( l  === 4  ) { d("odn", "odn.excite.co.jp", "search"); } else
-        if ( l  === 5  ) { d("eonet", "eonet.excite.co.jp", "search"); } else
-        if ( l  === 6  ) { d("excite", "excite.co.jp", "q"); } else
-        if ( l  === 7  ) { d("livedoor", "livedoor.com", "q"); } else
-        if ( l  === 8  ) { d("ocn", "ocn.ne.jp", "MT"); } else
-        if ( l  === 9  ) { d("j:com", "search.myjcom.jp", "q"); } else
-        if ( l  === 10 ) { d("sleipnir", "fenrir-inc.com", "q"); } else
-        if ( l  === 11 ) { d("lunascape", "luna.tv", "keyword"); } else
-        if ( l  === 12 ) { d("cybozu", "cybozu.net", "Keywords"); } else
-        if ( l  === 13 ) { d("sony", "sony.jp", "MT"); } else
-        if ( l  === 14 ) { d("fujitsu", "fmworld.net", "q"); } else
-        if ( l  === 15 ) { d("fujitsu", "fmworld.net", "Text"); } else
-        if ( l  === 16 ) { d("ecnavi", "ecnavi.jp", "Keywords"); } else
-        if ( l  === 17 ) { d("pex", "pex.jp", "Keywords"); } else
-        if ( l  === 18 ) { d("hao123", "hao123.com", "query"); } else
-        if ( l  === 19 ) { d("ceron", "ceron.jp", "q"); } else
-        if ( l  === 20 ) { d("お財布.com", "search.osaifu.com", "w"); } else
-        if ( l  === 21 ) { d("warau.jp", "warau.jp", "search_word"); } else
-        if ( l  === 22 ) { d("net-lavi", "netlavis.azione.jp", "q"); } else
-        if ( l  === 23 ) { d("genieo", "search.genieo.com", "q"); } else
-        if ( l  === 24 ) { d("funmoods search", "searchfunmoods.com", "q"); } else
-        if ( l  === 25 ) { d("fooooo", "fooooo.com", "q"); } else
-        if ( l  === 26 ) { d("便利コム", "benri.com", "Keywords"); } else
-        if ( l  === 27 ) { d("スマート検索", "smart-search.jp", "Keywords"); } else
-        if ( l  === 28 ) { d("jword", "jword.jp", "name"); } else
-        if ( l  === 29 ) { d("jword", "jword.jp", "q"); } else
-        if ( l  === 30 ) { d("aswidget", "aswidget.com", "Keywords"); } else
-        if ( l  === 31 ) { d("wow", "jp.wow.com", "q"); } else
-        if ( l  === 32 ) { d("adingo.jp", "adingo.jp", "Keywords"); } else
-        if ( l  === 33 ) { d("tnc", "search.tnc.ne.jp", "q"); } else
-        if ( l  === 34 ) { d("t-com", "search.t-com.ne.jp", "q"); } else
-        if ( l  === 35 ) { d("jiqoo", "search.jiqoo.jp", "q"); } else
-        if ( l  === 36 ) { d("auone", "sp-web.search.auone.jp", "q"); } else
-        if ( l  === 37 ) { d("green goo", "green.search.goo.ne.jp", "MT"); } else
-        {
-          $e.css({'color': '#23945C', 'font-weight': 'bold'}).text('登録が完了しました！');
-          $n.text(mv);
-          $c.text('閉じる');
-        }
-      } else {
-        // 実行が早すぎた時の処理
-        setTimeout(function() {
-          action();
-        }, s);
-      } // End - querySelector
+      // 動作開始
+      action();
 
-    } // End - action()
+      // キャンセル処理
+      $ga_cancel.on('click', function (e) {
+        e.preventDefault();
+        location.reload();
+      });
 
-  } // End - body hasClass
+    } // End - body hasClass
 
-} else {
+  } else {
 
-  alert('実行可能なページではありません。');
+    alert('実行可能なページではありません。');
 
-} // End - オーガニック検索ソース
+  } // End - オーガニック検索ソース
 
-})(jQuery)}if(typeof jQuery=='undefined'){var j=d.createElement('script');j.type='text/javascript';j.src='https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js';d.body.appendChild(j);j.onload=l}else{l()}})(document);
+})(jQuery)}if(typeof jQuery=='undefined'){var j=d.createElement('script');j.type='text/javascript';j.src='https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js';d.body.appendChild(j);j.onload=l}else{l()}})(document);
